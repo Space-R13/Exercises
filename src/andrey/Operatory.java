673c48228638563551d5dc7bb9c6790d0e84a8c4
Andrey <andrey1303@mail.ru>
2018-04-06 22:10:21 +0300

    3

---
+++ src/andrey/Operatory.java
@@ -0,0 +1,4 @@
+package andrey;
+
+public class Operatory {
+}
---
+++ src/andrey/Type.java
@@ -0,0 +1,13 @@
+package andrey;
+
+public class Type {
+    int a1 = -2147483648;
+    int a2 = 2147483647;
+    int a3 = 4;
+
+    short b1 = -32768;
+    short b2 = 32767;
+    short b3 = 2;
+
+
+}
