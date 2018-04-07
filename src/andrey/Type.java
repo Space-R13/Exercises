8db35d28f2be3db019413db195f51ca98c280cc3
Andrey <andrey1303@mail.ru>
2018-04-07 22:31:07 +0300

    3

--- src/andrey/Type.java
+++ src/andrey/Type.java
@@ -4,10 +4,31 @@ public class Type {
     int a1 = -2147483648;
     int a2 = 2147483647;
     int a3 = 4;
+    int a4 = 0b1001;
 
-    short b1 = -32768;
-    short b2 = 32767;
-    short b3 = 2;
+//    long b1 = -9223372036854775808;
+//    long b2 = -9223372036854775807;
+
+
+    short c1 = -32768;
+    short c2 = 32767;
+    short c3 = 2;
+
+    byte d1 = -128;
+    byte d2 = 127;
+    byte d3 = 1;
+
+    float e1 = -3.40282347E+38F;
+    float e2 = 3.40282347E+38F;
+    float e3 = 4;
+
+    double f1 = -1.7976931348623157E+308D;
+    double f2 = 1.7976931348623157E+308D;
+    double f3 = 8;
+
+
+
+
 
 
 }
