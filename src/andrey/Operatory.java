47c6e590c783c82c313b82da58630139fdb8000d
andrey1303@mail.ru <Githab1303>
2018-04-10 09:55:15 +0300

    123

--- src/andrey/Operatory.java
+++ src/andrey/Operatory.java
@@ -5,8 +5,10 @@ public class Operatory {
     int a2 = 2147483647;
     int a3 = 4;
     int a4 = 0b1001;
-    int a5 = 16;
+    int a5 = 3;
     int a6;
+    int a7 = 3;
+    int a8 = 4;
 
 
 //    long b1 = -9223372036854775808;
@@ -30,7 +32,7 @@ public class Operatory {
     double f1 = -1.7976931348623157E+308D;
     double f2 = 1.7976931348623157E+308D;
     double f3 = 8;
-    double f4 = 2;
+    double f4 = 3;
     double f5;
 
     char g1 = 69;
@@ -43,23 +45,34 @@ public class Operatory {
 
     Operatory(){
         a6 = (int) Math.sqrt(a5);
-        System.out.println(a6);
+        System.out.println(a6); // = 4
         a6 = (int)Math.pow(a3, a5);
-        System.out.println(a6);
+        System.out.println(a6); // = 64
         f5 = Math.pow(f3, f4);
-        System.out.println(f5);
+        System.out.println(f5); // = 512.0
         a6 = Math.floorMod(a3 + 1, 4);
-        System.out.println(a6);
+        System.out.println(a6); // = 1
         e4 = a5;
-        System.out.println(e4);
-        a6 = a3++;
-        System.out.println(a6);
-        a6 = ++a3;
-        System.out.println(a6);
-        a6 = a3--;
-        System.out.println(a6);
-        a6 = --a3;
-        System.out.println(a6);
+        System.out.println(e4); // = 3.0
+        a6 = 3 * a7++;
+        System.out.println(a6); // = 9
+        System.out.println(a7); // = 4
+        a6 = 3 * ++a8;
+        System.out.println(a6); // = 15
+        System.out.println(a8); // = 5
+        a6 = 3 * a7--;
+        System.out.println(a6); // = 12
+        System.out.println(a7); // = 3
+        a6 = 3 * --a8;
+        System.out.println(a6); // = 12
+        System.out.println(a8); // = 4
+        a6 = a7;
+        System.out.println(a6); // = 3
+        System.out.println(a7); // = 3
+        a6 = a8;
+        System.out.println(a6); // = 4
+        System.out.println(a8); // = 4
+
     }
 
 }
