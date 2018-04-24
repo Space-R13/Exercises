47adf4d6f94270d24b7ea16dc0221c61f47c904d
Andrey <andrey1303@mail.ru>
2018-04-24 20:49:02 +0300

    Merge branch 'master' of https://github.com/Space-R13/Exercises
    # Conflicts:
    #	src/andrey/Operatory.java

--- src/andrey/Main.java
+++ src/andrey/Main.java
@@ -3,7 +3,8 @@ package andrey;
 public class Main {
 
     public static void main(String[] args) {
-	new Cycle();
-	new Operatory();
+//	new Cycle();
+//	new Operatory();
+    new Massive();
     }
 }
